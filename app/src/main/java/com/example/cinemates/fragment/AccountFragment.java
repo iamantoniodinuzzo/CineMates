3c47d141474bf8b13dc7164a55dd67cdf2fa2757
Antonio <tonydinu02@gmail.com>
2021-12-19 10:58:35 +0100

    Aggiunto lo stile per le dialog
    + Implementate le dialog per la cancellazione dell'account e la disconnessione

--- app/src/main/java/com/example/cinemates/fragment/AccountFragment.java
+++ app/src/main/java/com/example/cinemates/fragment/AccountFragment.java
@@ -5,52 +5,37 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.cinemates.R;
-import com.example.cinemates.adapter.ConnectServiceAdapter;
 import com.example.cinemates.databinding.FragmentAccountBinding;
-import com.example.cinemates.model.ConnectService;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.example.cinemates.databinding.LayoutItemConnectServiceBinding;
 
 
-public class AccountFragment extends Fragment {
+public class AccountFragment extends Fragment implements ConfirmationDialogFragment.ConfirmationDialogListener {
 
     private FragmentAccountBinding mBinding;
-    private ConnectServiceAdapter mAdapter;
-    private List<ConnectService> mConnectServices;
+    private LayoutItemConnectServiceBinding mItemConnectServiceBinding;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mAdapter = new ConnectServiceAdapter();
-
-        initConnectServicesList();
-
 
     }
 
-    private void initConnectServicesList() {
-        mConnectServices = new ArrayList<>();
-        mConnectServices.add(new ConnectService("Sincronizza i dati dell'account", R.drawable.ic_round_autorenew_24, 1));
-        mConnectServices.add(new ConnectService("Elimina account", R.drawable.ic_baseline_delete_24, 1));
-        mConnectServices.add(new ConnectService("Disconnessione", R.drawable.ic_baseline_person_24, 1));
-
-        mAdapter.addItems(mConnectServices);
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         mBinding = FragmentAccountBinding.inflate(inflater, container, false);
-        mBinding.recyclerView.setAdapter(mAdapter);
+        mItemConnectServiceBinding = LayoutItemConnectServiceBinding.bind(mBinding.getRoot());
 
         return mBinding.getRoot();
     }
@@ -66,6 +51,35 @@ public class AccountFragment extends Fragment {
                 Navigation.findNavController(view).popBackStack();
             }
         });
+
+        mItemConnectServiceBinding.actionSyncAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO Sync account
+            }
+        });
+
+        mItemConnectServiceBinding.actionDeleteAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog("Delete account", "Sei sicuro di voler eliminare questo account?", "Si, eliminalo!", ConfirmationDialogFragment.ConfirmationTypo.DELETE);
+            }
+        });
+
+        mItemConnectServiceBinding.actionDisconnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog("Disconnection", "Sei sicuro di volerti disconnettere da questo account?", "Si, per favore!", ConfirmationDialogFragment.ConfirmationTypo.DISCONNECT);
+
+            }
+        });
+    }
+
+    private void showDialog(String title, String message, String positiveButton, ConfirmationDialogFragment.ConfirmationTypo typo) {
+        FragmentManager fm = getParentFragmentManager();
+        ConfirmationDialogFragment confirmationDialogFragment = ConfirmationDialogFragment.newInstance(title, message, positiveButton, typo);
+        confirmationDialogFragment.setTargetFragment(AccountFragment.this, 300);
+        confirmationDialogFragment.show(fm, "confirmation_dialog");
     }
 
     @Override
@@ -73,5 +87,19 @@ public class AccountFragment extends Fragment {
         super.onDestroyView();
         mBinding = null;
 
+    }
+
+    @Override
+    public void onFinisConfirmationDialog(ConfirmationDialogFragment.ConfirmationTypo typo) {
+        switch (typo) {
+            case DELETE:
+                //TODO delete account
+                Toast.makeText(getContext(), "Delete account", Toast.LENGTH_SHORT).show();
+                break;
+            case DISCONNECT:
+                //TODO disconnect account
+                Toast.makeText(getContext(), "Disconnecting", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }

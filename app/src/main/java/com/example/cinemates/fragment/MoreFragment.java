2c815cd047d43fdbf3706bc2a01d7b7203a0a99e
Antonio <tonydinu02@gmail.com>
2021-12-19 11:49:16 +0100

    Implementato il design del fragment more

--- app/src/main/java/com/example/cinemates/fragment/MoreFragment.java
+++ app/src/main/java/com/example/cinemates/fragment/MoreFragment.java
@@ -5,14 +5,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import com.example.cinemates.R;
 import com.example.cinemates.databinding.FragmentMoreBinding;
+import com.example.cinemates.databinding.ViewProfileBinding;
 
 
 public class MoreFragment extends Fragment {
 
     private FragmentMoreBinding mBinding;
+    private ViewProfileBinding mViewProfileBinding;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,7 +31,22 @@ public class MoreFragment extends Fragment {
                               ViewGroup container,
                               Bundle savedInstanceState) {
         mBinding = FragmentMoreBinding.inflate(inflater, container, false);
+        mViewProfileBinding = ViewProfileBinding.bind(mBinding.viewProfile.getRoot());
         return mBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        mViewProfileBinding.viewProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_moreFragment_to_accountFragment);
+            }
+        });
+
     }
 
     @Override

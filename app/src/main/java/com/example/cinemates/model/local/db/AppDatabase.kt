c641bfae96a51a991a73f5c0f4648e44af667408
jxareas <areasjx@gmail.com>
2022-08-29 00:52:54 -0600

    Refactor Gradle Build and Add Android KTX

--- app/src/main/java/com/example/cinemates/model/local/db/AppDatabase.kt
+++ app/src/main/java/com/example/cinemates/model/local/db/AppDatabase.kt
@@ -1,8 +1,8 @@
 package com.example.cinemates.model.local.db
 
 import androidx.room.Database
-import androidx.room.TypeConverters
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.cinemates.model.data.Movie
 import com.example.cinemates.model.data.Person
 import com.example.cinemates.model.local.dao.MovieDao
@@ -13,7 +13,7 @@ import com.example.cinemates.util.Converters
  * @author Antonio Di Nuzzo
  * Created 26/07/2022 at 08:13
  */
-@Database(entities = [Movie::class, Person::class], version = 11)
+@Database(entities = [Movie::class, Person::class], version = 11, exportSchema = false)
 @TypeConverters(
     Converters::class
 )

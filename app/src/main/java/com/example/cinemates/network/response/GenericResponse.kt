4d8568189af1e9b37c08e6338d15cfef8061089d
Antonio <tonydinu02@gmail.com>
2022-11-05 09:31:31 +0100

    Update GenericResponse.kt

--- app/src/main/java/com/example/cinemates/network/response/GenericResponse.kt
+++ app/src/main/java/com/example/cinemates/network/response/GenericResponse.kt
@@ -1,13 +1,15 @@
 package com.example.cinemates.network.response
 
+import kotlinx.serialization.SerialName
+
 /**
  * @author Antonio Di Nuzzo
- * Created 21/04/2022 at 15:39
  */
 class GenericResponse<T>(
     val page: Int?,
     @SerialName(value = "total_pages")
-    val total_pages: Int?,
-    val total_results: Int?,
+    val totalPages: Int?,
+    @SerialName(value = "total_results")
+    val totalResults: Int?,
     val results: List<T> = listOf()
 )

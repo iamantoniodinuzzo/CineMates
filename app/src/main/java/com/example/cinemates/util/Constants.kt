1f2400c3be39109cdf1e980eb35d3cf6ea35ac0b
Antonio <tonydinu02@gmail.com>
2022-12-31 11:16:51 +0100

    Update Constants.kt

--- app/src/main/java/com/example/cinemates/util/Constants.kt
+++ app/src/main/java/com/example/cinemates/util/Constants.kt
@@ -9,6 +9,7 @@ import com.example.cinemates.BuildConfig
 const val TMDb_API_KEY = BuildConfig.TMDB_API_KEY
 const val YT_API_KEY = BuildConfig.YT_API_KEY
 const val BASE_URL = "https://api.themoviedb.org/3/"
+const val DATABASE_NAME = "cineMate_db.db"
 
 const val IMAGE_BASE_URL_W780 = "https://image.tmdb.org/t/p/w780"
 const val IMAGE_BASE_URL_W500 = "https://image.tmdb.org/t/p/w500"

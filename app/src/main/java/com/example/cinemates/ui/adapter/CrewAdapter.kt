5536fdc1d1c91db2abb14070b79f56053fcf8fdf
Antonio <tonydinu02@gmail.com>
2023-03-28 08:05:43 +0200

    Update UI package structure

---
+++ app/src/main/java/com/example/cinemates/ui/adapter/CrewAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.cinemates.ui.adapter
+
+
+/**
+ * @author Antonio Di Nuzzo (Indisparte)
+ */
+import androidx.databinding.ViewDataBinding
+import androidx.navigation.Navigation
+import com.example.cinemates.NavGraphDirections
+import com.example.cinemates.R
+import com.example.cinemates.databinding.ListItemCrewLongBinding
+import com.example.cinemates.databinding.ListItemPersonSmallBinding
+import com.example.cinemates.model.Crew
+import com.example.cinemates.model.Person
+
+class CrewAdapter :
+    DoubleViewSizeAdapter<Crew, ListItemCrewLongBinding, ListItemPersonSmallBinding>(
+        R.layout.list_item_crew_long,
+        R.layout.list_item_person_small,
+        emptyList()
+    ) {
+
+
+    private fun navigateToDetails(binding: ViewDataBinding, item: Person) {
+        binding.root.setOnClickListener { view ->
+            val action = NavGraphDirections.actionGlobalActorDetailsFragment(item)
+            Navigation.findNavController(view).navigate(action)
+        }
+
+    }
+
+    override fun onBindLongItem(binding: ListItemCrewLongBinding, item: Crew) {
+        binding.crew = item
+        navigateToDetails(binding, item)
+    }
+
+    override fun onBindSmallItem(binding: ListItemPersonSmallBinding, item: Crew) {
+        binding.person = item
+        navigateToDetails(binding, item)
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/cinemates/ui/details/tvShow/TvSimilarFragment.kt
@@ -0,0 +1,29 @@
+package com.example.cinemates.ui.details.tvShow
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import com.example.cinemates.databinding.ListItemTvSmallBinding
+import com.example.cinemates.model.TvShow
+import com.example.cinemates.ui.ListFragment
+import com.example.cinemates.ui.adapter.TvShowAdapter
+
+class TvSimilarFragment : ListFragment<TvShow, ListItemTvSmallBinding, TvShowAdapter>(TvShowAdapter()) {
+
+    private val viewModel: TvDetailsViewModel by activityViewModels()
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewLifecycleOwner.lifecycleScope.launchWhenCreated {
+            viewModel.similarTvShow.collect { similar ->
+                adapter.updateItems(similar)
+                binding.counter = similar.size
+            }
+        }
+
+    }
+
+}

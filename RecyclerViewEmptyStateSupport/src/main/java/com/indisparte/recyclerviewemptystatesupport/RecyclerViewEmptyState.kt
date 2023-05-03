03ac01433b438154434e68c0b8db0eee9f2c4f95
Antonio <tonydinu02@gmail.com>
2023-05-03 10:19:08 +0200

    Update RecyclerViewEmptyState.kt

--- RecyclerViewEmptyStateSupport/src/main/java/com/indisparte/recyclerviewemptystatesupport/RecyclerViewEmptyState.kt
+++ RecyclerViewEmptyStateSupport/src/main/java/com/indisparte/recyclerviewemptystatesupport/RecyclerViewEmptyState.kt
@@ -9,19 +9,15 @@ import androidx.recyclerview.widget.RecyclerView
  *
  * @author Antonio Di Nuzzo (Indisparte)
  */
-internal class RecyclerViewEmptyState : RecyclerView {
+internal class RecyclerViewEmptyState(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int = 0
+) :RecyclerView(context, attrs, defStyle) {
+    constructor(context: Context) : this(context, null, 0)
+    constructor(context: Context, attrs: AttributeSet) : this(context, attrs, 0)
 
     private var emptyView: View? = null
-
-    constructor(context: Context?, attrs: AttributeSet?) : super(
-        context!!, attrs
-    )
-
-    constructor(context: Context?, attrs: AttributeSet?, defStyle: Int) : super(
-        context!!, attrs, defStyle
-    )
-
-    constructor(context: Context) : super(context)
 
     private val dataObserver: AdapterDataObserver = object : AdapterDataObserver() {
         override fun onChanged() {
@@ -41,15 +37,13 @@ internal class RecyclerViewEmptyState : RecyclerView {
     }
 
     override fun setAdapter(adapter: Adapter<*>?) {
-        val oldAdapter = getAdapter()
-        oldAdapter?.unregisterAdapterDataObserver(dataObserver)
+        getAdapter()?.unregisterAdapterDataObserver(dataObserver)
         super.setAdapter(adapter)
         adapter?.registerAdapterDataObserver(dataObserver)
         checkIfEmpty()
     }
 
-
-    fun setEmptyView(emptyView: View?) {
+    fun setEmptyView(emptyView: View) {
         this.emptyView = emptyView
         checkIfEmpty()
     }
@@ -62,17 +56,17 @@ internal class RecyclerViewEmptyState : RecyclerView {
     private fun checkIfEmpty() {
         val adapter = adapter
         val emptyView = emptyView
-        adapter?.let {
-            if (it.itemCount == 0) {
-                emptyView?.let { view ->
-                    view.visibility = View.VISIBLE
-                    this@RecyclerViewEmptyState.visibility = View.GONE
-                }
-            } else {
-                emptyView?.visibility = View.GONE
-                this@RecyclerViewEmptyState.visibility = View.VISIBLE
+
+        if (adapter != null && adapter.itemCount == 0) {
+            if (emptyView != null) {
+                emptyView.visibility = View.VISIBLE
             }
+            visibility = View.GONE
+        } else {
+            if (emptyView != null) {
+                emptyView.visibility = View.GONE
+            }
+            visibility = View.VISIBLE
         }
     }
-
 }

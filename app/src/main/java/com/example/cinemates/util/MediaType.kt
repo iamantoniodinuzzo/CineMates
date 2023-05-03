228e4dc3cca87c4e3152c82854986f5dc196c35c
Antonio <tonydinu02@gmail.com>
2023-05-03 11:45:54 +0200

    Fix (#204)

--- app/src/main/java/com/example/cinemates/util/MediaType.kt
+++ app/src/main/java/com/example/cinemates/util/MediaType.kt
@@ -7,7 +7,7 @@ package com.example.cinemates.util
 enum class MediaType(
     val value: String
 ) {
-    MOVIE("movie"), TV("tv"), PERSON("person");
+    MOVIE("movie"), TV("tv");
 
     override fun toString(): String {
         return value

0f5a8b854e3be6801c52c0c3eba9a30e8bb55a77
Antonio <tonydinu02@gmail.com>
2024-06-28 07:27:59 +0200

    Aggiornamenti build gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,8 +15,10 @@ plugins {
     id(libs.plugins.hilt.plugin.get().pluginId) version libs.versions.hiltPlugin apply false
     id(libs.plugins.kotlin.android.get().pluginId) version libs.versions.kotlin apply false
     id(libs.plugins.safeargs.get().pluginId) version libs.versions.navigation apply false
-//    alias(libs.plugins.android.library) apply false
-//    alias(libs.plugins.hilt.plugin) apply false
+    id(libs.plugins.kotlin.kapt.get().pluginId) version libs.versions.kotlin apply false
+//    id(libs.plugins.android.application.get().pluginId) version libs.versions.agp apply false
+//    id(libs.plugins.android.library.get().pluginId) version libs.versions.agp apply false
+
 }
 
 
